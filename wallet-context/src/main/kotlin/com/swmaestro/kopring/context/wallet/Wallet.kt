904fa5293eb42b1ef75755f742ae7469bd2d79b7
doubleu0714 <doubleu0714@Wonhyungui-MacBookPro.local>
2024-04-18 16:18:42 +0900

    리팩토링

--- wallet-context/src/main/kotlin/com/swmaestro/kopring/context/wallet/Wallet.kt
+++ wallet-context/src/main/kotlin/com/swmaestro/kopring/context/wallet/Wallet.kt
@@ -1,74 +1,72 @@
 package com.swmaestro.kopring.context.wallet
 
 import java.time.LocalDateTime
+import java.util.UUID
 
-class Wallet internal constructor(val balance: Int, val userId: String, val points: WalletPoints) {
+class Wallet internal constructor(
+    val guid: String,
+    val balance: Int,
+    val userId: String,
+    val points: WalletPoints
+) {
     fun charge(command: ChargeCommand): Result<Wallet> = runCatching {
-        Wallet(
+        applyWallet(
             balance = balance + command.amount,
-            userId = userId,
-            points = points.apply {
-                list.add(
-                    ChargedWalletPoint.create(
-                        command = ChargedWalletPoint.CreateCommand(
-                            chargedAmount = command.amount,
-                            chargedAt = LocalDateTime.now(),
-                        )
-                    ).getOrThrow()
+            point = ChargedWalletPoint.create(
+                command = ChargedWalletPoint.CreateCommand(
+                    chargedAmount = command.amount,
+                    chargedAt = LocalDateTime.now(),
                 )
-            }
+            ).getOrThrow()
         )
     }
 
     fun send(command: SendCommand): Result<Wallet> = runCatching {
-        Wallet(
+        applyWallet(
             balance = balance - command.amount,
-            userId = userId,
-            points = points.apply {
-                list.add(
-                    SentWalletPoint.create(
-                        command = SentWalletPoint.CreateCommand(
-                            sentAmount = command.amount,
-                            sentAt = LocalDateTime.now(),
-                        )
-                    ).getOrThrow()
+            point = SentWalletPoint.create(
+                command = SentWalletPoint.CreateCommand(
+                    sentAmount = command.amount,
+                    sentAt = LocalDateTime.now(),
                 )
-            }
+            ).getOrThrow()
         )
     }
 
     fun receive(command: ReceiveCommand): Result<Wallet> = runCatching {
-        Wallet(
+        applyWallet(
             balance = balance + command.amount,
-            userId = userId,
-            points = points.apply {
-                list.add(
-                    ReceivedWalletPoint.create(
-                        command = ReceivedWalletPoint.CreateCommand(
-                            receivedAmount = command.amount,
-                            receivedAt = LocalDateTime.now(),
-                        )
-                    ).getOrThrow()
+            point = ReceivedWalletPoint.create(
+                command = ReceivedWalletPoint.CreateCommand(
+                    receivedAmount = command.amount,
+                    receivedAt = LocalDateTime.now(),
                 )
-            }
+            ).getOrThrow()
         )
     }
+
     fun reject(command: RejectCommand): Result<Wallet> = runCatching {
-        Wallet(
+        applyWallet(
             balance = balance,
-            userId = userId,
-            points = points.apply {
-                list.add(
-                    RejectedWalletPoint.create(
-                        command = RejectedWalletPoint.CreateCommand(
-                            rejectedAmount = command.amount,
-                            rejectedAt = LocalDateTime.now(),
-                        )
-                    ).getOrThrow()
+            point = RejectedWalletPoint.create(
+                command = RejectedWalletPoint.CreateCommand(
+                    rejectedAmount = command.amount,
+                    rejectedAt = LocalDateTime.now(),
                 )
-            }
+            ).getOrThrow()
         )
     }
+
+    private fun applyWallet(balance: Int, point: WalletPoint) = Wallet(
+        guid = guid,
+        balance = balance,
+        userId = userId,
+        points = points.apply {
+            list.add(
+                point
+            )
+        }
+    )
 
     data class CreateCommand(val userId: String)
     data class ChargeCommand(val amount: Int)
@@ -79,6 +77,7 @@ class Wallet internal constructor(val balance: Int, val userId: String, val poin
     companion object {
         fun create(command: CreateCommand): Result<Wallet> = runCatching {
             Wallet(
+                guid = UUID.randomUUID().toString(),
                 balance = 0,
                 userId = command.userId,
                 points = WalletPoints.empty()
