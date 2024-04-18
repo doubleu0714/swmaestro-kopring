6e0a0982e9ef8c78ba02c10d3e6992421e402481
doubleu0714 <doubleu0714@Wonhyungui-MacBookPro.local>
2024-04-18 15:53:48 +0900

    지갑 구현 with TDD

--- wallet-context/src/main/kotlin/com/swmaestro/kopring/context/wallet/Wallet.kt
+++ wallet-context/src/main/kotlin/com/swmaestro/kopring/context/wallet/Wallet.kt
@@ -1,7 +1,81 @@
 package com.swmaestro.kopring.context.wallet
 
+import java.time.LocalDateTime
+
 class Wallet internal constructor(val balance: Int, val userId: String, val points: WalletPoints) {
+    fun charge(command: ChargeCommand): Result<Wallet> = runCatching {
+        Wallet(
+            balance = balance + command.amount,
+            userId = userId,
+            points = points.apply {
+                list.add(
+                    ChargedWalletPoint.create(
+                        command = ChargedWalletPoint.CreateCommand(
+                            chargedAmount = command.amount,
+                            chargedAt = LocalDateTime.now(),
+                        )
+                    ).getOrThrow()
+                )
+            }
+        )
+    }
+
+    fun send(command: SendCommand): Result<Wallet> = runCatching {
+        Wallet(
+            balance = balance - command.amount,
+            userId = userId,
+            points = points.apply {
+                list.add(
+                    SentWalletPoint.create(
+                        command = SentWalletPoint.CreateCommand(
+                            sentAmount = command.amount,
+                            sentAt = LocalDateTime.now(),
+                        )
+                    ).getOrThrow()
+                )
+            }
+        )
+    }
+
+    fun receive(command: ReceiveCommand): Result<Wallet> = runCatching {
+        Wallet(
+            balance = balance + command.amount,
+            userId = userId,
+            points = points.apply {
+                list.add(
+                    ReceivedWalletPoint.create(
+                        command = ReceivedWalletPoint.CreateCommand(
+                            receivedAmount = command.amount,
+                            receivedAt = LocalDateTime.now(),
+                        )
+                    ).getOrThrow()
+                )
+            }
+        )
+    }
+    fun reject(command: RejectCommand): Result<Wallet> = runCatching {
+        Wallet(
+            balance = balance,
+            userId = userId,
+            points = points.apply {
+                list.add(
+                    RejectedWalletPoint.create(
+                        command = RejectedWalletPoint.CreateCommand(
+                            rejectedAmount = command.amount,
+                            rejectedAt = LocalDateTime.now(),
+                        )
+                    ).getOrThrow()
+                )
+            }
+        )
+    }
+
     data class CreateCommand(val userId: String)
+    data class ChargeCommand(val amount: Int)
+    data class SendCommand(val amount: Int)
+    data class ReceiveCommand(val amount: Int)
+    data class RejectCommand(val amount: Int)
+
     companion object {
         fun create(command: CreateCommand): Result<Wallet> = runCatching {
             Wallet(
--- wallet-context/src/test/kotlin/com/swmaestro/kopring/context/wallet/WalletTest.kt
+++ wallet-context/src/test/kotlin/com/swmaestro/kopring/context/wallet/WalletTest.kt
@@ -1,50 +1,89 @@
 package com.swmaestro.kopring.context.wallet
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class WalletTest {
     @Test
     fun `지갑을 생성한다`() {
         // given
+        val command = Wallet.CreateCommand(userId = "userId")
 
         // when
+        val actual: Result<Wallet> = Wallet.create(command = command)
 
         // then
+        actual.onSuccess {
+            assertEquals(0, it.balance)
+            assertEquals(command.userId, it.userId)
+            assertEquals(0, it.points.size)
+        }
     }
 
     @Test
     fun `충전을 하면 지갑의 잔액이 증가하고 충전 포인트가 추가된다`() {
         // given
+        val sut: Wallet = Wallet.create(command = Wallet.CreateCommand(userId = "userId")).getOrThrow()
+        val command = Wallet.ChargeCommand(amount = 100)
 
         // when
+        val actual: Result<Wallet> = sut.charge(command = command)
 
         // then
+        actual.onSuccess {
+            assertEquals(command.amount, it.balance)
+            assertTrue { it.points[0] is ChargedWalletPoint }
+        }
     }
 
     @Test
     fun `전송하면 지갑의 잔액이 줄고 전송 포인트가 추가된다`() {
         // given
+        val sut: Wallet = Wallet.create(command = Wallet.CreateCommand(userId = "userId")).getOrThrow()
+        sut.charge(command = Wallet.ChargeCommand(amount = 100))
+        val command = Wallet.SendCommand(amount = 50)
 
         // when
+        val actual: Result<Wallet> = sut.send(command = command)
 
         // then
+        actual.onSuccess {
+            assertEquals(sut.balance - command.amount, it.balance)
+            assertTrue { it.points[0] is ChargedWalletPoint }
+            assertTrue { it.points[1] is SentWalletPoint }
+        }
     }
 
     @Test
     fun `수신하면 지갑의 잔액이 증가하고 수신 포인트가 추가된다`() {
         // given
+        val sut: Wallet = Wallet.create(command = Wallet.CreateCommand(userId = "userId")).getOrThrow()
+        val command = Wallet.ReceiveCommand(amount = 100)
 
         // when
+        val actual: Result<Wallet> = sut.receive(command = command)
 
         // then
+        actual.onSuccess {
+            assertEquals(command.amount, it.balance)
+            assertTrue { it.points[0] is ReceivedWalletPoint }
+        }
     }
 
     @Test
     fun `거절하면 지갑의 잔액은 변경되지 않고 거절 포인트 추가된다`() {
         // given
+        val sut: Wallet = Wallet.create(command = Wallet.CreateCommand(userId = "userId")).getOrThrow()
+        val command = Wallet.RejectCommand(amount = 100)
 
         // when
+        val actual: Result<Wallet> = sut.reject(command = command)
 
         // then
+        actual.onSuccess {
+            assertEquals(sut.balance, it.balance)
+            assertTrue { it.points[0] is RejectedWalletPoint }
+        }
     }
 }
