82437794475cd711a95ae07c1c757f42050a1fb6
doubleu0714 <doubleu0714@Wonhyungui-MacBookPro.local>
2024-04-18 16:09:45 +0900

    guid 추가

--- wallet-context/src/main/kotlin/com/swmaestro/kopring/context/wallet/WalletPoint.kt
+++ wallet-context/src/main/kotlin/com/swmaestro/kopring/context/wallet/WalletPoint.kt
@@ -1,14 +1,17 @@
 package com.swmaestro.kopring.context.wallet
 
 import java.time.LocalDateTime
+import java.util.*
 
 interface WalletPoint {
+    val guid: String,
     val amount: Int
     val createdAt: LocalDateTime
     val description: String
 }
 
 class ChargedWalletPoint internal constructor(
+    override val guid: String,
     override val amount: Int,
     override val createdAt: LocalDateTime
 ) : WalletPoint {
@@ -17,40 +20,68 @@ class ChargedWalletPoint internal constructor(
     data class CreateCommand(val chargedAmount: Int, val chargedAt: LocalDateTime)
     companion object {
         fun create(command: CreateCommand): Result<ChargedWalletPoint> = runCatching {
-            ChargedWalletPoint(amount = command.chargedAmount, createdAt = command.chargedAt)
+            ChargedWalletPoint(
+                guid = UUID.randomUUID().toString(),
+                amount = command.chargedAmount,
+                createdAt = command.chargedAt
+            )
         }
     }
 }
 
-class SentWalletPoint internal constructor(override val amount: Int, override val createdAt: LocalDateTime) : WalletPoint{
+class SentWalletPoint internal constructor(
+    override val guid: String,
+    override val amount: Int,
+    override val createdAt: LocalDateTime
+) : WalletPoint {
     override val description: String = "$amount 포인트 전송 $createdAt"
 
     data class CreateCommand(val sentAmount: Int, val sentAt: LocalDateTime)
     companion object {
         fun create(command: CreateCommand): Result<SentWalletPoint> = runCatching {
-            SentWalletPoint(amount = command.sentAmount, createdAt = command.sentAt)
+            SentWalletPoint(
+                guid = UUID.randomUUID().toString(),
+                amount = command.sentAmount,
+                createdAt = command.sentAt
+            )
         }
     }
 }
 
-class ReceivedWalletPoint internal constructor(override val amount: Int, override val createdAt: LocalDateTime) : WalletPoint{
+class ReceivedWalletPoint internal constructor(
+    override val guid: String,
+    override val amount: Int,
+    override val createdAt: LocalDateTime
+) : WalletPoint {
     override val description: String = "$amount 포인트 수신 $createdAt"
 
     data class CreateCommand(val receivedAmount: Int, val receivedAt: LocalDateTime)
     companion object {
         fun create(command: CreateCommand): Result<ReceivedWalletPoint> = runCatching {
-            ReceivedWalletPoint(amount = command.receivedAmount, createdAt = command.receivedAt)
+            ReceivedWalletPoint(
+                guid = UUID.randomUUID().toString(),
+                amount = command.receivedAmount,
+                createdAt = command.receivedAt
+            )
         }
     }
 }
 
-class RejectedWalletPoint internal constructor(override val amount: Int, override val createdAt: LocalDateTime) : WalletPoint{
+class RejectedWalletPoint internal constructor(
+    override val guid: String,
+    override val amount: Int,
+    override val createdAt: LocalDateTime
+) : WalletPoint {
     override val description: String = "$amount 포인트 거절 $createdAt"
 
     data class CreateCommand(val rejectedAmount: Int, val rejectedAt: LocalDateTime)
     companion object {
         fun create(command: CreateCommand): Result<RejectedWalletPoint> = runCatching {
-            RejectedWalletPoint(amount = command.rejectedAmount, createdAt = command.rejectedAt)
+            RejectedWalletPoint(
+                guid = UUID.randomUUID().toString(),
+                amount = command.rejectedAmount,
+                createdAt = command.rejectedAt
+            )
         }
     }
 }
